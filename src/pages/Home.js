465d3d1b85a1a97081881146715228d390a2ddec
Yuken Tezuka <tezuka.y@icloud.com>
2020-06-13 16:48:12 +0900

    finish

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Text, View, TouchableOpacity, StyleSheet, Modal } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Modal,
+  ImageBackground,
+  SafeAreaView,
+} from "react-native";
 import MyModal from "./Modal";
 import { vegetables } from "../vegetables/vegetables";
 
@@ -8,6 +16,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    opacity: 1.0,
+  },
+  text: {
+    fontSize: 45,
+    color: "#fff",
+  },
+  button: {
+    backgroundColor: "#556b2f",
+    padding: 19,
+    borderRadius: 9,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    opacity: 0.8,
+  },
+  safeArea: {
+    backgroundColor: "khaki",
   },
 });
 
@@ -28,14 +54,28 @@ export default function Home() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text>home</Text>
-      <TouchableOpacity onPress={() => selectVege()}>
-        <Text>Open Modal</Text>
-      </TouchableOpacity>
-      <Modal animationType="slide" transparent={true} visible={modalVisible}>
-        <MyModal onPress={setModalVisible} vege1={vege1} vege2={vege2} />
-      </Modal>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <ImageBackground
+        source={require("../../assets/food-1898194_1920.jpg")}
+        style={styles.image}
+      >
+        <View style={styles.container}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={styles.button}
+            onPress={() => selectVege()}
+          >
+            <Text style={styles.text}>自炊する</Text>
+          </TouchableOpacity>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+          >
+            <MyModal onPress={setModalVisible} vege1={vege1} vege2={vege2} />
+          </Modal>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
--- src/pages/Modal.js
+++ src/pages/Modal.js
@@ -11,9 +11,43 @@ const styles = StyleSheet.create({
   modal: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#ddd",
+    backgroundColor: "#daa520",
     width: 300,
     height: 300,
+    borderRadius: 10,
+  },
+  vege: {
+    fontSize: 45,
+    marginBottom: 10,
+    marginTop: 10,
+    color: "#fff",
+    borderWidth: 1,
+    borderColor: "#fff",
+    borderRadius: 8,
+    padding: 3,
+  },
+  text1: {
+    fontSize: 25,
+    position: "absolute",
+    top: 30,
+    color: "#fff",
+  },
+  text2: {
+    fontSize: 25,
+    color: "#fff",
+    position: "absolute",
+    bottom: 40,
+  },
+  close: {
+    fontSize: 20,
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    backgroundColor: "#ddd",
+    borderRadius: 8,
+  },
+  closeText: {
+    padding: 3,
   },
 });
 
@@ -21,10 +55,12 @@ export default function Modal({ onPress, vege1, vege2 }) {
   return (
     <View style={styles.container}>
       <View style={styles.modal}>
-        <Text>{vege1}</Text>
-        <Text>{vege2}</Text>
-        <TouchableOpacity onPress={() => onPress(false)}>
-          <Text>close</Text>
+        <Text style={styles.text1}>今日の食材は...</Text>
+        <Text style={styles.vege}>{vege1}</Text>
+        <Text style={styles.vege}>{vege2}</Text>
+        <Text style={styles.text2}>グッドラック！</Text>
+        <TouchableOpacity style={styles.close} onPress={() => onPress(false)}>
+          <Text style={styles.closeText}>close</Text>
         </TouchableOpacity>
       </View>
     </View>
